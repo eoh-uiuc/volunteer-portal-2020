733d33ddfcc33799c89f649e201e393986bee1f9
Lauren Ho <laurenh4@illinois.edu>
2020-05-30 19:18:05 -0500

    Add society selector

--- frontend/src/pages/CreateAccount/index.js
+++ frontend/src/pages/CreateAccount/index.js
@@ -1,7 +1,41 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Select from 'react-select';
 
-import "../Login/styles.scss"; // Uses same styling as login page
+import '../Login/styles.scss'; // Uses same styling as login page
+
+const societies = [
+  { value: 'None', label: 'None' },
+  { value: 'Aerospace Outreach', label: 'Aerospace Outreach' },
+  { value: 'Alma\'s Talking Dogs', label: 'Alma\'s Talking Dogs' },
+  { value: 'Alpha Chi Sigma', label: 'Alpha Chi Sigma' },
+  { value: 'Alpha Omega Epsilon', label: 'Alpha Omega Epsilon' },
+  { value: 'Alpha Pi Mu', label: 'Alpha Pi Mu' },
+  { value: 'American Chemical Society', label: 'American Chemical Society' },
+  { value: 'American Concrete Institute', label: 'American Concrete Institute' },
+  { value: 'American Institute of Aeronautics and Astronautics', label: 'American Institute of Aeronautics and Astronautics' },
+  { value: 'American Institute of Chemical Engineers', label: 'American Institute of Chemical Engineers' },
+  { value: 'American Nuclear Society', label: 'American Nuclear Society' },
+]
+
+const societySelectorStyles = {
+  control: (base, state) => ({
+    ...base,
+    boxShadow: 'none',
+    borderColor: state.isFocused? '#a3a3a3' : '#ccc',
+    borderRadius: 7, 
+    cursor: 'text',
+    '&:hover': { borderColor: 'none' }
+  }),
+  dropdownIndicator: (base, state) => ({
+    ...base,
+    '&:hover': { cursor: 'pointer' },
+  }),
+  option: (base, state) => ({
+    ...base,
+    '&:hover': { cursor: 'pointer' }
+  })
+};
 
 class CreateAccount extends Component {
   constructor(props) {
@@ -32,6 +66,8 @@ class CreateAccount extends Component {
                 <input id="auth-field" type="text" />
                 <p>Password</p>
                 <input id="auth-field" type="password" />
+                <p>Society</p>
+                <Select styles={societySelectorStyles} options={societies} />
                 <br />
                 <input id="auth-button" type="submit" value="Create Account" onClick={this.handleCreateAccount} />
             </form>
